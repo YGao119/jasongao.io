2fb536a8e6744d8f1c5f81efc67a60c8af9964b4
YGao119 <jasongao@usc.edu>
2022-12-20 15:54:30 -0800

    add project link

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -8,19 +8,25 @@ export const ProjectList = [
         name: "PokerSC",
         image: Proj1,
         skills: "ReactJS, Java Spring Boot, RESTful API",
-        link: "https://github.com/csci201atFall2021-finalproject",
+        link: "http://pokersc.pro/",
+        github: "https://github.com/csci201atFall2021-finalproject",
+        intro: "9-max Texas No-Limit Hold’em. It supports multiplayers and real-time audio chatting. Enjoy the convenience of having fun with your friends without face-to-face contact in this post-pandemic time period. Fairness is our priority. Fisher–Yates shuffle algorithm guarantees each deal is uniformly-randomly made. No form of cheating is possible. Want to watch how your friends play poker while not actually betting? Join the watching room! You can see the board while players’ own hands are kept private."
     },
     {
         name: "Easy Team Up",
         image: Proj2,
         skills: "NodeJS, Android, MySQL, Retrofit",
-        link: "https://github.com/YGao119/Easy-Team-Up",
+        link: "",
+        github: "https://github.com/YGao119/Easy-Team-Up",
+        intro: "How do you find a partner to form a study group or do sports together? Post into different chat groups or message others one by one? No matter which, it is going to make the organizing work laborious and difficult to manage. Even more, people who might be interested might be dismissed because they missed the chat message or you don’t even know them. And you, at the same time, lose a chance to meet with a new friend.\n         To facilitate this situation, in this project, we are going to create an Android app that provides a platform for students to team up or gather together. In this app, users can post invitations for their events and everybody else could see and join it. This is going to help students manage their social events more easily and also expose them to more social opportunities. Following are the features that need to be implemented in this project. Each group member is responsible for implementing one of the three main features, but implementing the basic one is everyone’s responsibility."
     },
     {
         name: "Network of Servers",
         image: Proj3,
         skills: "Computer Networking, Peer to Peer Architecture, Routing Algorithm",
         link: "",
+        github: "",
+        intro: "It is an application-level network with Peer-to-Peer architecture, layered on top of the Internet. It comes with link layer and network layer functionalities such as forwarding and flooding using the hard-state approach of link state routing algorithm. More importantly, the network is customized for both UDP-like and TCP-like applications by providing UDT and RDT-3.0 transport layer services."
     }
     
 ]
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
 import {ProjectList} from '../helpers/ProjectList';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faGithub} from '@fortawesome/free-brands-svg-icons'
@@ -9,11 +9,20 @@ export default function ProjectDisplay() {
     const {id} = useParams();
     const project = ProjectList[id];
     return (
-        <div className='project'>
-            <h1>{project.name}</h1>
-            <img src={project.image} alt='Project Demo'></img>
-            <p> <b> Skills:</b> {project.skills}</p>
-            <a href={project.link} target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub}/></a>
-        </div>
+        <>
+            <div className='project'>
+                <h1>{project.name}</h1>
+                <a href={project.link} target="_blank" rel="noreferrer noopener">
+                    <img src={project.image} alt='Project Demo'></img>
+                </a>
+                <p> <b> Skills:</b> {project.skills}</p>
+                <a href={project.github} target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub}/></a>
+            </div>
+            <div className='projectIntro'>
+                <h1>Introduction</h1>
+                <p>{project.intro}</p>
+            </div>
+        </>
+        
     )
 }
